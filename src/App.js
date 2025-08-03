a9dfc8880bbeab0568da977c4f12f1679ec71a9d
Amit0841 <aroy16063@gmail.com>
2025-08-03 19:35:52 +0530

    game test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 import LevelSelector from "./LevelSelector";
 import GamePage from "./GamePage";
 import ResultPage from "./ResultPage";
